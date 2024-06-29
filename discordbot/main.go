ed195b952ecf772558a4a9cdf8955c146c551bc2
raccoon-mh <wsx1341@gmail.com>
2024-06-29 01:53:55 +0000

    update git

--- discordbot/main.go
+++ discordbot/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,28 +9,16 @@ import (
 	"raccoon-mh-playground/handler"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
 )
 
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-}
-
 func main() {
-	discord, err := discordgo.New("Bot " + "")
+	DISCORD_BOT_TOKEN := os.Getenv("DISCORD_BOT_TOKEN")
+	discord, err := discordgo.New("Bot " + DISCORD_BOT_TOKEN)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	discord.AddHandler(handler.MessageCreate)
+	discord.AddHandler(handler.PingPong)
 
-	// Just like the ping pong example, we only care about receiving message
-	// events in this example.
-	// discord.Identify.Intents = discordgo.IntentsGuildMessages
-
-	// Open a websocket connection to Discord and begin listening.
 	err = discord.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
@@ -44,6 +31,5 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
-	// Cleanly close down the Discord session.
 	discord.Close()
 }
